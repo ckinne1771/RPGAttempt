f4e876d2fd20b5a47f8727fad50f7cbe3a5e5273
Matthew Mitchell <matt.mancer@gmail.com>
2015-04-27 14:48:59 +0100

    Clean up

---
+++ RPG Test Project/Assets/Scripts/BattleScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleScript : MonoBehaviour {
+	public enum BattleState {
+		START,
+		PLAYERCHOICE,
+		ENEMYCHOICE,
+		LOSE,
+		WIN
+	}
+
+	private BattleState currentState = BattleState.START;
+
+	void Update() {
+		switch (currentState) {
+		case BattleState.START:
+			//SETUP
+			break;
+		case BattleState.PLAYERCHOICE:
+			//Player Choice
+			break;
+		case BattleState.ENEMYCHOICE:
+			//Enemy turn
+			break;
+		case BattleState.LOSE:
+			//Lost Battle
+			break;
+		case BattleState.WIN:
+			//Won Battle
+			break;
+		}
+	}
+}
---
+++ RPG Test Project/Assets/Scripts/PartyMemberScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyMemberScript : MonoBehaviour {
+
+	[SerializeField] private int hitPoints;
+	[SerializeField] private int manaPoints;
+	[SerializeField] private int attackPoints;
+	[SerializeField] private int defensePoints;
+	[SerializeField] private int magicAttackPoints;
+	[SerializeField] private int magicDefensePoints;
+	[SerializeField] private int speed;
+}
